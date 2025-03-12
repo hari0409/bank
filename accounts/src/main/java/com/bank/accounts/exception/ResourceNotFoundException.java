092681a9f701dc3675fbff81822353143b4b2fa1
hari0409 <hari626007@gmail.com>
2025-03-12 23:48:59 +0530

    Complete Individual Services.

--- accounts/src/main/java/com/bank/accounts/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bank.accounts.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resoruceName, String fieldName, String fieldValue){
-        super(String.format("%s not found with the given input data %s: %s",resoruceName, fieldName, fieldValue));
-    }
-}
