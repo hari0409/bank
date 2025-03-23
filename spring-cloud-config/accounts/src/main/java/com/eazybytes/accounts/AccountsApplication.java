a9a0bc319b52ffc59528560fa1d892cdba9c99da
hari0409 <hari626007@gmail.com>
2025-03-24 01:06:22 +0530

    Finishing off with H2 to MySQL Migration.

--- spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts;
 
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -10,12 +11,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.eazybytes.accounts.config.ConfigHandler;
-
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@OpenAPIDefinition(info = @Info(title = "Accounts microservice REST API Documentation", description = "EazyBank Accounts microservice REST API Documentation", version = "v1", contact = @Contact(name = "Madan Reddy", email = "tutor@eazybytes.com", url = "https://www.eazybytes.com"), license = @License(name = "Apache 2.0", url = "https://www.eazybytes.com")), externalDocs = @ExternalDocumentation(description = "EazyBank Accounts microservice REST API Documentation", url = "https://www.eazybytes.com/swagger-ui.html"))
-@EnableConfigurationProperties(value = { ConfigHandler.class })
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@eazybytes.com",
+						url = "https://www.eazybytes.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.eazybytes.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
